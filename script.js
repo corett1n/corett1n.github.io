e1a52d28cf29d9c81c363acaf9937f335c3ef98e
corett1n <martin.coretti@gmail.com>
2024-01-06 15:08:33 +0100

    New

--- script.js
+++ script.js
@@ -2307,6 +2307,18 @@ let turisticnedestinacijeQuizOptions = [
   "toplice",
   "Triglavski narodni park",
   "Velika planina",
+  "Sečovlje",
+  "Kranj",
+  "Novo mesto",
+  "Triglav",
+  "Ptuj",
+  "Škofja Loka",
+  "Logarska dolina",
+  "Portorož",
+  "Izola",
+  "Mesečev zaliv",
+  "Cerkniško jezero",
+  "Pohorje",
 ];
 let turisticnedestinacijeQuiz = [
   {
@@ -2428,6 +2440,102 @@ let turisticnedestinacijeQuiz = [
     correct: turisticnedestinacijeQuizOptions[14],
     questionImg:
       "media/turisticnedestinacije/slovenija-turisticnedestinacije-velikaplanina.png",
+  },
+  {
+    id: "15",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[15],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-secovlje.png",
+  },
+  {
+    id: "16",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[16],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-kranj.png",
+  },
+  {
+    id: "17",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[17],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-novomesto.png",
+  },
+  {
+    id: "18",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[18],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-triglav.png",
+  },
+  {
+    id: "19",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[19],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-ptuj.png",
+  },
+  {
+    id: "20",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[20],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-skofjaloka.png",
+  },
+  {
+    id: "21",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[21],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-logarskadolina.png",
+  },
+  {
+    id: "22",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[22],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-portoroz.png",
+  },
+  {
+    id: "23",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[23],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-izola.png",
+  },
+  {
+    id: "24",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[24],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-mesecevzaliv.png",
+  },
+  {
+    id: "25",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[25],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-cerkniskojezero.png",
+  },
+  {
+    id: "26",
+    question: "",
+    options: turisticnedestinacijeQuizOptions,
+    correct: turisticnedestinacijeQuizOptions[26],
+    questionImg:
+      "media/turisticnedestinacije/slovenija-turisticnedestinacije-pohorje.png",
   },
 ];
 
